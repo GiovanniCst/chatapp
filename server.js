421fbf43ccc5b8d952f2c5129ed9c3433626758e
Giovanni J. Costantini <g.costantini@gmail.com>
2016-12-31 16:48:16 +0100

    Developing the server - more helperFunct

--- server.js
+++ server.js
@@ -21,3 +21,50 @@ function sendFile(response, filePath, fileContents) {
     response.writeHead(200, {'Content-Type': mime.lookup(path.basename(filePath))});
     response.end(fileContents);
 }
+
+// Helper function - Cache dei file statici
+
+function seveStatic(response, cache, absPath) {
+    // Il file è già in memoria?
+    if (cache[absPath]) {
+        // Servi il file dalla memoria
+        sendFile(response, absPath, cache[absPath]);
+    } else {
+        // Controlla se il file esiste
+        fs.exists(absPath, function (exists) {
+            if (exists) {
+                // Leggi il file dal disco
+                fs.readFile(absPath, function (err, data) {
+                    if (err) {
+                        send404(response);
+                    } else {
+                        // Servi il file che hai letto dal disco
+                        cache[absPath] = data;
+                        sendFile(response, absPath, data);
+                    }
+                });
+            } else {
+                // Risorsa non trovata
+                send404(response);
+            }
+        });
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
