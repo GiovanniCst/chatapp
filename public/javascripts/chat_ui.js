af815256600d5af102760caa95578885d404ddae
Giovanni J. Costantini <g.costantini@gmail.com>
2017-01-02 22:56:28 +0100

    minor edit

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -15,10 +15,10 @@ function processUserInput(chatApp, socket) {
     var message = $('#send-message').val();
     var systemMessage;
     // Se l'input dell'utente comincia con /, è un comando
-    if (message.charAt(0) === '/') {
+    if (message.charAt(0) == '/') {
         systemMessage = chatApp.processCommand(message);
         if (systemMessage) {
-            $('#messages').append(divEscapedContentElement(systemMessage));
+            $('#messages').append(divSystemContentElement(systemMessage));
         } else {
             //Broadcast il messaggio a tutti gli utenti
             chatApp.sendMessage($('#room').text(), message);
